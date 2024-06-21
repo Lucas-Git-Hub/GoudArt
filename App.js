786175020acc6db7be74ce5cc1c92aa941775c3f
Lucas-Git-Hub <lvanwaas@hotmail.com>
2024-06-21 13:50:56 +0200

    actual getTheme fix

--- App.js
+++ App.js
@@ -77,6 +77,7 @@ const App = () => {
     if(sights.length === 0){ //Check if already fetched/list filled
       getLocationData();
     }
+    getTheme();
     askPermissionForUserLocation();
   }, [])
 
@@ -85,8 +86,6 @@ const App = () => {
     if(resetData === true){
       getTheme();
       setResetData(false);
-    } else {
-      getTheme();
     }
   }, [resetData])
   
